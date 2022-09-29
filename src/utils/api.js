fca791d057cb1b00006732ece5f3a13082ffbb18
sam-c-f <sam_friedman@hotmail.co.uk>
2022-09-29 10:26:28 +0100

    removed next from catch

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,34 +16,26 @@ export const getArticles = async (topic, sort_by, order_by, limit, page) => {
       },
     });
     return data;
-  } catch (err) {
-    next(err);
-  }
+  } catch (err) {}
 };
 
 export const getArticleById = async (article_id) => {
   try {
     const { data } = await newsApi.get(`/articles/${article_id}`);
     return data;
-  } catch (err) {
-    next(err);
-  }
+  } catch (err) {}
 };
 
 export const patchArticleVotes = async (article_id, reqBody) => {
   try {
     const { data } = await newsApi.patch(`/articles/${article_id}`, reqBody);
     return data;
-  } catch (err) {
-    next(err);
-  }
+  } catch (err) {}
 };
 
 export const getTopics = async () => {
   try {
     const { data } = await newsApi.get("/topics");
     return data;
-  } catch (err) {
-    next(err);
-  }
+  } catch (err) {}
 };
